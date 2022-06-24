78150c6a36423b51910caf3ffc38b497608430b9
allibrdoine <allibrodine81@gmail.com>
2022-06-24 18:55:45 -0500

    display first question/start timer

--- assets/script.js
+++ assets/script.js
@@ -3,35 +3,46 @@ const questionContainerEl = document.querySelector("#question-container");
 const questionEl = document.querySelector("#question");
 const answerButtons = document.querySelector("#answers");
 const begin = document.querySelector("#begin");
+const timeLeft = document.getElementById("time-left");
 
 //Array of questions
 const questions = [
 
     { question: "HTML uses _____",
         answers: [
-            {a: "user defined tags.", correct: false },
-            {b: "fixed tags defined by the language.", correct: true },
-            {c: "tags for links only.", correct: false },
-            {d: "predefined tags.", correct: false }
-        ]},
+            `a: user defined tags.`,
+            `b: fixed tags defined by the language.`,
+            `c: tags for links only.`,
+            `d: predefined tags.`
+        ],
+            correctAnswer: `b: fixed tags defined by the language.` 
+    },
 
     { question: "If we want to set the style for just one element with CSS, which CSS selector will we use?",
         answers: [
-            {a: "text", correct: false },
-            {b: "class", correct: false },
-            {c: "name", correct: false },
-            {d: "id", correct: true }
-        ]},
+            `a: text`,
+            `b: class`,
+            `c: name`,
+            `d: id`
+        ],
+            correctAnswer: `d: id`
+    },
 
     { question: "Which of the following keywords is used to define a variable in JavaScript?",
         answers: [
-            {a: "let", correct: false },
-            {b: "var", correct: false },
-            {c: "none", correct: false },
-            {d: "both A and B", correct: true }
-        ]},
+            `a: let`, 
+            `b: var`, 
+            `c: none`,
+            `d: both A and B` 
+        ],
+            correctAnswer: `d: both A and B` 
+    },
 
 ];
+
+let timerId;
+let remainingTime = 30;
+let questionIndex = 0;
 
 //Start quiz
 startButton.addEventListener('click', startGame)
@@ -39,21 +50,58 @@ startButton.addEventListener('click', startGame)
 function startGame() {
     console.log("started")
     //hide start screen
-    //begin.classList.add('hide');
-    begin.setAttribute("class", "hide");
+    //begin.classList.add('hide'); - didn't work
+    //begin.setAttribute("class", "hide"); - didn't work
+    begin.style.display = 'none';
+
+    //start timer
+    timerId = setInterval(tick, 1000);
+    timeLeft.textContent = remainingTime;
+
+    //display question container
+    questionContainerEl.style.display = "block";
+    
+    //call question
+    getNextQuestion();
 };
 
-//startGame();
+function getNextQuestion() {
 
-//display questions, collect answers
-function displayQuestions() {
+    //load question
+    let currentQuestion = questions[questionIndex];
+    questionEl.textContent = currentQuestion.question;
+
+    //load answers
+    let btn1El = document.getElementById("btn1");
+    btn1El.textContent = currentQuestion.answers[0];
+    let btn2El = document.getElementById("btn2");
+    btn2El.textContent = currentQuestion.answers[1];
+    let btn3El = document.getElementById("btn3");
+    btn3El.textContent = currentQuestion.answers[2];
+    let btn4El = document.getElementById("btn4");
+    btn4El.textContent = currentQuestion.answers[3];
+
+    //for loop to cycle through questions
 
 }
 
-displayQuestions();
+//collect answers
 
 
 //timer
+function tick() {
+    remainingTime--;
+    timeLeft.textContent = remainingTime;
+
+    if (remainingTime <= 0) {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    timeLeft.textContent = '';
+    clearInterval(timeLeft);
+}
 
 //handle scoring
 
